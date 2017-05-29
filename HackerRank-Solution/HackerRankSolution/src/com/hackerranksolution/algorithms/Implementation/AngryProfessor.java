6994a6c3f326dda7c4de3f2df04b69fdd4673cb4
brighterapi <milindmehta01@gmail.com>
2017-05-29 22:08:46 +0530

    BAPI-17 Hackerrank Angry Professor Solution

---
+++ HackerRank-Solution/HackerRankSolution/src/com/hackerranksolution/algorithms/Implementation/AngryProfessor.java
@@ -0,0 +1,33 @@
+package com.hackerranksolution.algorithms.Implementation;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Milind
+ */
+public class AngryProfessor {
+    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int a0 = 0; a0 < t; a0++){
+            int n = in.nextInt();
+            int k = in.nextInt();
+            int a[] = new int[n];
+            int count = 0;
+            for(int a_i=0; a_i < n; a_i++){
+                a[a_i] = in.nextInt();
+                if(a[a_i]<=0){
+                    count++;
+                }
+            }
+            if(count>=k){
+                System.out.println("NO");
+            }else{
+                System.out.println("YES");
+            }
+        }
+    }
+    
+}
