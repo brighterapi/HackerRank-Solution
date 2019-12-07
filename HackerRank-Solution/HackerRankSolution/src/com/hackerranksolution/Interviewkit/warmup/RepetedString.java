5d18ed60cc2756f50a6ae28086219d0fc42d2a38
brighterapi <milindmehta01@gmail.com>
2019-12-07 22:27:28 +0530

    RepetedString https://www.hackerrank.com/challenges/repeated-string/problem?h_l=interview&playlist_slugs%5B%5D%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D%5B%5D=warmup&isFullScreen=true

---
+++ HackerRank-Solution/HackerRankSolution/src/com/hackerranksolution/Interviewkit/warmup/RepetedString.java
@@ -0,0 +1,35 @@
+package com.hackerranksolution.Interviewkit.warmup;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class RepetedString {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new
+                InputStreamReader(System.in));
+        String string = br.readLine();
+        long n = Long.parseLong(br.readLine());
+        System.out.println(repetedString(string, n));
+    }
+
+    public static long repetedString(String str, long n) {
+        long multiply = (n / str.length());
+        int aCount = 0;
+        char[] stringCharArray = str.toCharArray();
+        for (Character ch : stringCharArray) {
+            if (ch == 'a') {
+                aCount++;
+            }
+        }
+        long totalCount = multiply * aCount;
+        long remaining = n - (multiply * stringCharArray.length);
+        for (int i = 0; i < remaining; i++) {
+            if (str.charAt(i) == 'a') {
+                totalCount++;
+            }
+        }
+        return totalCount;
+    }
+}
